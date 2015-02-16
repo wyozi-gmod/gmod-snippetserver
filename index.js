e806a966bfe0aa2aff9b24a90224621ab0a8ee65
wyozi <joonasmertanen@gmail.com>
2015-02-16 19:34:11 +0200

    Use json for snippets

--- index.js
+++ index.js
@@ -1,30 +1,28 @@
 var express = require('express');
-var app = express();
+var fs = require('fs');
+var db = require('full-text-search-light');
 
+// Create web server
+var app = express();
 app.set('port', (process.env.PORT || 5000));
 
-var db = require('full-text-search-light');
+// Create full text search for snippets
 var snippetDb = new db('snippets');
 snippetDb.init();
 
 // Load snippets
-var snippetExports = require('require-dir')('./snippets');
 var snippets = {};
 
-for (var nm in snippetExports) {
-    var ex = snippetExports[nm];
-
-    if (!ex.snippets) {
-        continue;
-    }
-
-    ex.snippets.forEach(function(snip) {
+var snippetFiles = fs.readdirSync("./snippets");
+snippetFiles.forEach(function(fil) {
+    var snippets = JSON.parse(fs.readFileSync("./snippets/" + fil));
+    snippets.forEach(function(snip) {
         var id = snippetDb.add(snip);
 
         snip.id = id;
         snippets[id] = snip;
     });
-}
+});
 
 app.get('/snippets', function(request, response) {
     response.send(snippetDb.search(request.query.query));
