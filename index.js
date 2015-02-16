820ee05b5dba64212bb219694f53bd7dccb352d4
wyozi <joonasmertanen@gmail.com>
2015-02-16 19:27:57 +0200

    Implement basic search

--- index.js
+++ index.js
@@ -3,13 +3,31 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.get('/snippets/:query', function(request, response) {
-    response.send({
-        responses: [
-            { val: "Hello" },
-            { val: "World" }
-        ]
+var db = require('full-text-search-light');
+var snippetDb = new db('snippets');
+snippetDb.init();
+
+// Load snippets
+var snippetExports = require('require-dir')('./snippets');
+var snippets = {};
+
+for (var nm in snippetExports) {
+    var ex = snippetExports[nm];
+
+    if (!ex.snippets) {
+        continue;
+    }
+
+    ex.snippets.forEach(function(snip) {
+        var id = snippetDb.add(snip);
+
+        snip.id = id;
+        snippets[id] = snip;
     });
+}
+
+app.get('/snippets', function(request, response) {
+    response.send(snippetDb.search(request.query.query));
 });
 
 app.listen(app.get('port'), function() {
---
+++ snippets/file.js
@@ -0,0 +1,14 @@
+module.exports.snippets = [
+    {
+        title: 'Read file',
+        author: 'Wyozi',
+        desc: 'Reads file "file.txt" from the data folder',
+        code: 'file.Read("file.txt", "DATA")'
+    },
+    {
+        title: 'Write file',
+        author: 'Wyozi',
+        desc: 'Writes "Hello" to "file.txt" in the data folder',
+        code: 'file.Write("file.txt", "Hello")'
+    }
+];
