10b09e145f9a18033f50463fa92232db9da06bf1
wyozi <joonasmertanen@gmail.com>
2015-02-16 19:05:37 +0200

    Prepare github

--- index.js
+++ index.js
@@ -4,14 +4,14 @@ var app = express();
 app.set('port', (process.env.PORT || 5000));
 
 app.get('/snippets/:query', function(request, response) {
-  response.send({
-      responses: [
-          { val: "Hello" },
-          { val: "World" }
-      ]
-  });
+    response.send({
+        responses: [
+            { val: "Hello" },
+            { val: "World" }
+        ]
+    });
 });
 
 app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'));
+    console.log("Node app is running at localhost:" + app.get('port'));
 });
